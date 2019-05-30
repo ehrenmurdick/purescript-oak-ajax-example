73b0d00bb566efb772b4e2317cd9974256be8245
Ehren Murdick <ehren.murdick@gmail.com>
2019-05-30 14:50:16 -0700

    updated to make request on page load

--- src/Main.purs
+++ src/Main.purs
@@ -6,15 +6,18 @@ import Prelude
   , map
   , mempty
   , show
+  , (<<<)
+  , (>>>)
   , (<>)
   , ($)
   )
 import Style as S
 import Oak.Html.Events (onClick, onInput)
 import Oak.Html.Attribute ( style, value )
-import Oak.Debug ( debugApp )
+import Oak.Debug ( DebugMsg(..), debugApp )
 import Data.Show (class Show)
 import Data.Either (Either(..))
+import Data.Maybe
 import Effect (Effect)
 import Oak
   ( runApp
@@ -61,14 +64,17 @@ data Msg
   = Get
   | Input String
   | Got Response
+  | Post
 
 data Response
   = Index (Either AjaxError Index)
+  | PostR (Either AjaxError Todo)
 
 instance showMsg :: Show Msg where
   show msg =
     case msg of
       Get -> "Get"
+      Post -> "Post"
       Got r -> "Got " <> show r
       Input str -> "Input"
 
@@ -78,6 +84,8 @@ instance showResponse :: Show Response where
     case resp of
       Index (Right _) -> "Index"
       Index e -> "Index " <> show e
+      PostR (Right d) -> "Post " <> show d
+      PostR e -> "Post " <> show e
 
 
 view :: Model -> Html Msg
@@ -93,6 +101,7 @@ view model =
     , section [ style $ S.big <> S.section ]
       [ div [ style S.big ]
         [ input [ onInput Input, value model.form.text ] []
+        , button [ onClick Post ] [ text "create" ]
         ]
       ]
     ]
@@ -105,19 +114,36 @@ showTodo todo =
 urlFor :: String -> String
 urlFor str = "http://localhost:3000/todos" <> str
 
+postBody :: Model -> Maybe { text :: String }
+postBody model =
+  Just $ { text: model.form.text }
 
 next :: Msg -> Model -> (Msg -> Effect Unit) -> Effect Unit
 next msg mod h =
   case msg of
     Got _ -> mempty
     Input _ -> mempty
-    Get -> get Index (urlFor "") (map h Got)
+    Get -> do
+      get
+        (urlFor "")
+        (Index >>> Got >>> h)
+    Post -> do
+      post
+          (postBody mod)
+          (urlFor "")
+          (PostR >>> Got >>> h)
 
 update :: Msg -> Model -> Model
 update msg model =
   case msg of
     Get -> model
+    Post -> model
     Got (Index (Right todos)) -> model { todos = todos }
+    Got (PostR (Right todo)) ->
+      model
+        { todos = model.todos <> [ todo ]
+        , form { text = "" }
+        }
     Got _ -> model
     Input str -> model { form { text = str } }
 
@@ -138,6 +164,6 @@ app = createApp
 
 main :: Effect Unit
 main = do
-  rootNode <- runApp (debugApp app)
+  rootNode <- runApp (debugApp app) (Just (Wrap Get))
   container <- getElementById "app"
   appendChildNode container rootNode
